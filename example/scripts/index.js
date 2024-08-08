e51059a5403ed7c51debd3533d64763c2abf786b
oscar <jos.nu02@gmail.com>
2024-08-07 20:37:03 -0500

    learning javascript

---
+++ example/scripts/index.js
@@ -0,0 +1,9 @@
+let nombre ="oscar";
+const selector = document.getElementById("mensaje");
+/* escribir dentro de esa etiqueta con JS */
+prompt("Preguntar algo");
+console.log("Imprimir algo");
+const entrada = prompt("¿Qué producto desea comprar?");
+console.log(entrada);
+alert(entrada);
+const mensaje = "Bienvenido " + nombre;
